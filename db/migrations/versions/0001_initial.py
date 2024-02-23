682c32ccc4d648007254cd216db5882f41bf4b03
Annovid <nil.nil532@gmail.com>
2024-02-23 23:14:42 +0300

    add first migration

--- db/migrations/versions/0001_initial.py
+++ db/migrations/versions/0001_initial.py
@@ -11,8 +11,34 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(64), nullable=False),
+        sa.Column('timestamp', sa.DateTime, nullable=True)
+    )
+
+    op.create_table(
+        'titles',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(64), nullable=False),
+        sa.Column('timestamp', sa.DateTime, nullable=True),
+        sa.Column('name_rus', sa.String(64), nullable=False),
+        sa.Column('rate', sa.String(10), nullable=False),
+        sa.Column('episodes', sa.Integer, nullable=True),
+        sa.Column('kind', sa.String(20), nullable=False),
+        sa.Column('status', sa.String(20), nullable=False)
+    )
+
+    op.create_table(
+        'user_rates',
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('users.id'), primary_key=True),
+        sa.Column('title_id', sa.Integer, sa.ForeignKey('titles.id'), primary_key=True),
+        sa.Column('rate', sa.Integer, nullable=False)
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table('user_rates')
+    op.drop_table('titles')
+    op.drop_table('users')
